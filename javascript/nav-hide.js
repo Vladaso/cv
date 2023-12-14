c261d9882f438740595ba1f18d20dfbe325bb50d
Vladimir Jakabovic <vladimir.jakabovic@gmail.com>
2023-12-14 15:16:27 +0100

    nav fix

--- javascript/nav-hide.js
+++ javascript/nav-hide.js
@@ -14,12 +14,14 @@ var prevScrollpos = window.scrollY;
         var currentScrollPos = window.scrollY;
         if (!buttonPressed) {
             var navbar = document.getElementById("navbar");
+            if(Math.abs(prevScrollpos - currentScrollPos) > 100){
+                if (prevScrollpos > currentScrollPos) {
+                    navbar.style.top = "0";
+                } else {
+                    navbar.style.top = "-20%";
+                }
+                prevScrollpos = currentScrollPos;
 
-            if (prevScrollpos-15 > currentScrollPos) {
-                navbar.style.top = "0";
-            } else {
-                navbar.style.top = "-20%";
             }
-            prevScrollpos = currentScrollPos;
         }
     });
